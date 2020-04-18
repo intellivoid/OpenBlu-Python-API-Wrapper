ee690f7eb011b4a08f5aa433f6f89546ba158600
nocturn9x <hackhab@gmail.com>
2020-04-18 08:54:48 +0000

    core module done

--- openblu/core/openblu.py
+++ openblu/core/openblu.py
@@ -4,6 +4,7 @@ import json
 import os
 import logging
 from ..objects import Server, ServerListing
+from ..errors import OpenBluError, UnauthorizedAccess, ServerNotFound
 
 
 class OpenBluAPI:
@@ -15,6 +16,7 @@ class OpenBluAPI:
     """
 
     API_ENDPOINT = "https://api.intellivoid.net/openblu/v1"   # API endpoint for OpenBlu
+    ERROR_CODES = defaultdict(lambda: OpenBluError, {401: UnauthorizedAccess, 404: ServerNotFound})
 
     def __init__(self, access_key: str):
         """Object constructor"""
@@ -24,8 +26,26 @@ class OpenBluAPI:
         self.key = access_key
 
     def __repr__(self):
+        """Implements repr(self)
+           Follows the convention that eval(repr(self)) == self
+        """
+
         return f"OpenBluAPI('{self.key}')"
 
+
+    def __str__(self):
+        """Implements str(self)"""
+
+        return f"<openblu.OpenBluAPI object at {hex(id(self))}, key='{self.key}'"
+
+    def _raise_exception(self, code: int):
+        """Raises the appropriate exception based on the status code
+
+           :param code: The status code of the request
+           :type code: int
+        """
+
+        raise self.ERROR_CODES[code]
 
     def get_server(uuid: str, verbose: bool = False):
         """Fetches a single OpenVPN server from the OpenBlu API, given its unique ID
@@ -36,6 +56,7 @@ class OpenBluAPI:
 	   :type verbose: bool, optional
 	   :returns: A class:Server object
 	   :rtype: class: Server
+           :raises OpenBluError: An proper subclass of OpenBluError is raised if something goes wrong. If the error cannot be determined, a generic OpenBluError is raised
         """
 
         link = self.API_ENDPOINT + "/servers/get"
@@ -47,10 +68,10 @@ class OpenBluAPI:
         post_data["id"] = uuid
         response = requests.post(link, data=post_data)
         data = json.loads(response.text)
-        if data["success"]:
+        if data["response_code"] == 200:
             return Server(data)
         else:
-        
+            self._raise_exception(data["response_code"])
 
 
     def fetch_servers(filter_by: Union[None, tuple] = None, order_by: Union[None, str] = None, sort_by: Union[None, str] = None, verbose: bool = False):
@@ -68,6 +89,7 @@ class OpenBluAPI:
 	   :type verbose: bool, optional
 	   :returns servers_list: A class class:ServerListing object
 	   :rtype servers_list: class: ServerListing
+           :raises OpenBluError: An proper subclass of OpenBluError is raised if something goes wrong. If the error cannot be determined, a generic OpenBluError is raised
         """
 
         link = self.API_ENDPOINT + "/servers/list"
@@ -84,5 +106,9 @@ class OpenBluAPI:
         if sort_by:
             post_data["sort_by"] = sort_by
         response = requests.post(link, data=post_data)
-        return ServerListing(json.loads(response.text))
+        data = json.loads(response.text)
+        if data["response_code"] == 200:
+            return ServerListing(data)
+        else:
+            self._raise_exception(data["status_code"])
 
